0696dec07d348abd25645c5f8c0905d9783b06f3
Santiago Roa <52055041+SantiagoRoa@users.noreply.github.com>
2021-12-13 00:56:56 -0500

    Filtro añadido

--- App.py
+++ App.py
@@ -9,13 +9,24 @@ client = Socrata("www.datos.gov.co", None)
 results = client.get("sdvb-4x4j", limit=2000)
 df = pd.DataFrame.from_records(results)
 
-app = Flask(__name__)
+tablaOriginal = df.rename(columns={'num_resolucion': 'Número de resolución', 'fecha_resolucion': 'Fecha de resolución'
+, 'a_o':'Año', 'cod_territorio':'Código de territorio', 'nom_territorio':'Departamento', 'laboratorio_vacuna':'Laboratorio', 'cantidad':'Dosis aplicadas'
+, 'uso_vacuna':'Población vacunada', 'fecha_corte':'Fecha de registro'})
 
+app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+    return render_template('index.html', tables=[tablaOriginal.to_html(classes='data')], titles=tablaOriginal.columns.values)
+
 
 def html_table():
-    return render_template('index.html')    
+    return render_template('index.html')
+
+
+def filtrar(param, value):
+    param = 'num_resolucion'
+    value = '555'
+    tablaFiltrada = df[df[param] == value]
+    return render_template('index.html', result=tablaFiltrada)
